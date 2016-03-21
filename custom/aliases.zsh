65ed6535aefdc7f2d4c3a66a8645bf25d52bfefa
yliangsiew <yliangsiew@blizzard.com>
2016-03-21 16:23:13 -0700

    -added aliases and environment variables specifically for work

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -6,11 +6,11 @@ alias gl=git log --oneline --all --graph --decorate $*
 
 # Blizzard aliases
 
-# Application aliases
+## Application aliases
 alias kill_maya="pkill maya"
 alias charm="/home/yliangsiew/Applications/pycharm-community-5.0.4/bin/pycharm.sh"
 alias pycharm5="/home/yliangsiew/Applications/pycharm-community-5.0.4/bin/pycharm.sh"
+alias qmake="/usr/lib64/qt4/bin/qmake $*"
 
 ## Build aliases
-
-alias buildp='python $BFD_DEV_ROOT/bfdlaunch/tools/bfdpkgPublish.py -d -f 1 -z 0 -o "/corp.blizzard.net/BFD/Farm/Public/$USER/packages" $*'
+alias buildp='bl -p=build blpkgpub -d -f 1 -z 0 -o "/corp.blizzard.net/BFD/Farm/Public/$USER/packages" $*'

901c00cbf40dd9eb27bdb2423a828d3577c33308
yliangsiew <yliangsiew@blizzard.com>
2016-03-22 11:57:05 -0700

    disabled crash reports being sent automatically

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,7 +1,9 @@
 # Custom environment variables
 # These are additional environent variables that are set by the end-user.
+export PYTHONSTARTUP=~/.pythonrc
 
+# Blizzard-specific environment variables
 export BFD_DEV_ROOT=/home/yliangsiew/svn/dev/Pipeline/Trunk
 export BFD_DEPLOYABLES_ROOT=/home/yliangsiew/svn/dev/Deployables/Trunk
 export BFD_PKG_ROOT=$HOME/src/pkg
-export PYTHONSTARTUP=~/.pythonrc
+export BFD_DEV_ENABLE_EXCEPTION_REPORTING=0

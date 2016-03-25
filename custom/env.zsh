a3c4f5c34c2f0f95a32d81ed58421d2a3cba721e
yliangsiew <yliangsiew@blizzard.com>
2016-03-25 15:49:09 -0700

    -added custom environment variable for enabling bfdlogger

--- custom/env.zsh
+++ custom/env.zsh
@@ -2,9 +2,3 @@
 # These are additional environent variables that are set by the end-user.
 export PATH=$PATH:/home/yliangsiew/.local/bin
 export PYTHONSTARTUP=~/.pythonrc
-
-# Blizzard-specific environment variables
-export BFD_DEV_ROOT=/home/yliangsiew/svn/dev/Pipeline/Trunk
-export BFD_DEPLOYABLES_ROOT=/home/yliangsiew/svn/dev/Deployables/Trunk
-export BFD_PKG_ROOT=$HOME/src/pkg
-export BFD_DEV_ENABLE_EXCEPTION_REPORTING=0

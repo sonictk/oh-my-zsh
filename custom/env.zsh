4bb26fc4152c85f9792ea71294b51e49b0ddc1a2
yliangsiew <yliangsiew@blizzard.com>
2016-04-13 09:49:09 -0700

    -Added env file for setting default editor environment variable

---
+++ custom/env.zsh
@@ -0,0 +1,5 @@
+# Custom environment variables
+# These are additional environent variables that are set by the end-user.
+export PYTHONSTARTUP=~/.pythonrc
+export VISUAL=vim
+export EDITOR="$VISUAL"

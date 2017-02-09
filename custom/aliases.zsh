978125bcbe045a102f96d5d6a1984a401f5e36f1
yliangsiew <yliangsiew@blizzard.com>
2017-02-09 09:54:52 -0800

    updated zbfd setup script and aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -26,5 +26,5 @@ alias npm="/home/yliangsiew/bin/node-v5.9.1-linux-x64/bin/npm"
 # alias firefox="/home/yliangsiew/bin/firefox/firefox" # Firefox 45 search bar is not working properly, need to fix it before attempting to re-enable this alias
 
 ## Build aliases
-alias buildpackage='bl -p=build blpkgpub -d -f 1 -z 0 -o "/corp.blizzard.net/BFD/Farm/Public/$USER/packages" $*'
+alias buildpackage='bl -p=build blpkgpub -d -f -z -o "/corp.blizzard.net/BFD/Farm/Public/$USER/packages" $*'
 alias updatepipeline="bl -p=build blpipeup $*"
--- custom/env.zsh
+++ custom/env.zsh
@@ -4,3 +4,4 @@ export PATH=$PATH:/home/yliangsiew/.local/bin
 export PYTHONSTARTUP=~/.pythonrc
 export VISUAL=vim
 export EDITOR="$VISUAL"
+export GPGKEY=yliangsiew@blizzard.com

fbd9d26d6b73a84985c7218e109d4f8a24dc6806
sonictk <admin@sonictk.com>
2017-02-12 14:27:00 -0800

    added alias for maya

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -8,5 +8,6 @@ alias git ls-files | xargs cat | wc -l
 alias gl=git log --oneline --all --graph --decorate $*
 
 ## Application aliases
+alias maya="/Applications/Autodesk/maya2016/Maya.app/Contents/bin/maya"
 alias kill_maya="pkill maya"
 alias kill_mari="pkill MriBin"

d8b212efd35f3a2b05e47cec3c3f3e1f16650757
yliangsiew <yliangsiew@blizzard.com>
2017-10-25 17:31:04 -0700

    fixed buildpackage alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -26,6 +26,6 @@ alias npm="/home/yliangsiew/bin/node-v5.9.1-linux-x64/bin/npm"
 # alias firefox="/home/yliangsiew/bin/firefox/firefox" # Firefox 45 search bar is not working properly, need to fix it before attempting to re-enable this alias
 
 ## Build aliases
-alias buildpackage='bl -p=build blpkgpub -d -f -n $* "/corp.blizzard.net/BFD/Farm/Public/$USER/packages"'
+buildpackage() { bl -p=build blpkgpub -d -f -n "$1" "/corp.blizzard.net/BFD/Farm/Public/$USER/packages"; }
 alias updatepipeline="bl -p=build blpipeup $*"
 alias gitr="/home/yliangsiew/bin/node-v5.9.1-linux-x64/lib/node_modules/gitr/bin/gitr.js $*"
